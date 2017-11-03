3f8468162aae2566d7ab4a96c6ee150faa23472c
Pulkit Maloo <maloop@iu.edu>
2017-11-02 23:58:57 -0400

    updated get-dois

--- get-dois/get-dois.py
+++ get-dois/get-dois.py
@@ -8,7 +8,6 @@ Created on Sat Oct 28 02:34:09 2017
 import xml.etree.ElementTree as ET
 
 infile = 'Isaac_Citations_Library.xml'
-search_string_outfile = 'titles_search.txt'
 
 tree = ET.parse(infile)
 root = tree.getroot()
@@ -19,17 +18,35 @@ dois = []
 titles = []
 errors = 0
 
-
 for record in records:
     try:
-        #        dois.append(record.find('electronic-resource-num').find('style').text)
+#        dois.append(record.find('electronic-resource-num').find('style').text)
         titles.append(record.find('titles').find('title').find('style').text)
     except:
         errors += 1
 
 print len(titles), "titles found"
 
+with open("titles.txt", 'w') as writefile:
+    writefile.write('\n'.join(titles).encode('utf-8'))
+
 titles_search = " OR ".join(['TITLE("' + title + '")' for title in titles])
 
-with open(search_string_outfile, 'w') as writefile:
+with open('titles_search.txt', 'w') as writefile:
     writefile.write(titles_search.encode('utf-8'))
+
+'''
+After running the script above:
+* Open `titles_search.txt`.  Copy and paste the search string into Scopus advanced search:
+    http://www-scopus-com/search/form.url?zone=TopNavBar&origin=searchadvanced
+
+* Scopus returns 174 results.
+
+* In the search results page, select "Select All", then "View Cited By".
+
+* Scopus returns 911 results.
+
+* Select "Export" > "CSV" and "Specify fields to be exported" > "DOI" only, then "Export".
+
+* Exported file saved as `gen_1 2015-10-30.csv`.
+'''
